9522ad2e78efde9826e2f38023445370b2775540
Robert A Dixon <robmsc24@gmail.com>
2024-08-26 17:00:29 -0400

    wip

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,22 +1,37 @@
+import React, { useCallback } from 'react';
 import PropTypes from 'prop-types';
 
 export default function TodoInput(props) {
-    const { handleAddTodos, todoValue, setTodoValue } = props
-    return (
-        <header>
-            <input value={todoValue} onChange={(e) => {
-                setTodoValue(e.target.value)
-            }} placeholder="Enter todo..." />
-            <button onClick={() => {
-                handleAddTodos(todoValue)
-                setTodoValue('')
-            }}>Add</button>
-        </header>
-    )
+  const { handleAddTodos, todoValue, setTodoValue } = props;
+
+  const handleAddTodo = useCallback(() => {
+    if (todoValue.trim() === '') {
+      alert('Needs a todo');
+    } else {
+      handleAddTodos(todoValue);
+      setTodoValue('');
+    }
+  }, [todoValue, handleAddTodos, setTodoValue]);
+
+  const handleInputChange = useCallback((e) => {
+    setTodoValue(e.target.value);
+  }, [setTodoValue]);
+
+  return (
+    <header>
+      <input 
+        value={todoValue} 
+        onChange={handleInputChange} 
+        placeholder="Enter todo..." 
+      />
+      <button onClick={handleAddTodo}>Add</button>
+    </header>
+  );
 }
 
 TodoInput.propTypes = {
-    handleAddTodos: PropTypes.func.isRequired,
-    todoValue: PropTypes.string.isRequired,
-    setTodoValue: PropTypes.func.isRequired,
-  };
+  handleAddTodos: PropTypes.func.isRequired,
+  todoValue: PropTypes.string.isRequired,
+  setTodoValue: PropTypes.func.isRequired,
+};
+
