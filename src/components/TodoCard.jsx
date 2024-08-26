2a8d0661d763f7afd9729ef75831adf58f73620a
Robert A Dixon <robmsc24@gmail.com>
2024-08-26 15:57:18 -0400

    wip

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -1,22 +1,34 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
 
 export default function TodoCard(props) {
-  const { children, handleDeleteTodo, index, handleEditTodo } = props
+  const { children, handleDeleteTodo, index, handleEditTodo, todo } = props;
+
+
+
   return (
     <li className="todoItem">
-      {children}
+      {children || todo}
       <div className="actionsContainer">
         <button onClick={() => {
-          handleEditTodo(index)
-          }}>
-        <i className="fa-solid fa-pen-to-square"></i>
+          handleEditTodo(index);
+        }}>
+          <i className="fa-solid fa-pen-to-square"></i>
         </button>
         <button onClick={() => {
-          handleDeleteTodo(index)
+          handleDeleteTodo(index);
         }}>
-        <i className="fa-regular fa-trash-can"></i>
+          <i className="fa-regular fa-trash-can"></i>
         </button>
       </div>
     </li>
   );
 }
+
+TodoCard.propTypes = {
+  children: PropTypes.node,  // Optional - allows passing content as children
+  handleDeleteTodo: PropTypes.func.isRequired,
+  index: PropTypes.number.isRequired,
+  handleEditTodo: PropTypes.func.isRequired,
+  todo: PropTypes.string.isRequired, // New prop for the actual todo content
+};
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,19 +1,25 @@
 import React from 'react'
 import TodoCard from './TodoCard'
+import PropTypes from 'prop-types';
 
 export default function TodoList(props) {
-    const { todos } = props
-   
+  const { todos } = props
+
+  PropTypes.checkPropTypes({
+    todos: PropTypes.arrayOf(PropTypes.string).isRequired
+  }, props);
 
   return (
     <ul className='main'>
-      {todos.map((todo, todoIndex) => {
-          return (
-              <TodoCard {...props} key={todoIndex} index={todoIndex}>
-                  <p>{todo}</p>
-              </TodoCard>
-          )
-      })}
+      {todos.map((todo, todoIndex) => (
+        <TodoCard key={todoIndex} index={todoIndex} {...props}>
+          <p>{todo}</p>
+        </TodoCard>
+      ))}
     </ul>
   )
 }
+
+TodoList.propTypes = {
+  todos: PropTypes.arrayOf(PropTypes.string).isRequired
+};
