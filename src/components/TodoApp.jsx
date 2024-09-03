b8090252dd8fe8bba3c542449c3119fee3447d98
Robert A Dixon <robmsc24@gmail.com>
2024-09-03 00:52:48 -0400

    wip

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,37 +1,37 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { addTodo, toggleTodo, editTodo, removeTodo } from '../redux/todoSlice';
+import { addTodo, toggleTodo, editTodo, removeTodo } from '../redux/slices/todoSlice';
 import TodoInput from './TodoInput';
 import TodoList from './TodoList';
 
 const TodoApp = () => {
-  const todos = useSelector((state) => state.todos);
+  const todos = useSelector((state) => state.todos.items); // Access todos from state
   const dispatch = useDispatch();
 
   const handleAddTodo = (text) => {
-    dispatch(addTodo(text));
+    dispatch(addTodo(text)); // Dispatch addTodo action
   };
 
   const handleToggleTodo = (id) => {
-    dispatch(toggleTodo(id));
+    dispatch(toggleTodo(id)); // Dispatch toggleTodo action
   };
 
   const handleEditTodo = (id, newText) => {
-    dispatch(editTodo({ id, text: newText }));
+    dispatch(editTodo({ id, text: newText })); // Dispatch editTodo action
   };
 
   const handleRemoveTodo = (id) => {
-    dispatch(removeTodo(id));
+    dispatch(removeTodo(id)); // Dispatch removeTodo action
   };
 
   return (
     <div>
       <TodoInput onAddTodo={handleAddTodo} />
-      <TodoList 
-        todos={todos} 
-        onToggleTodo={handleToggleTodo} 
-        onEditTodo={handleEditTodo} 
-        onRemoveTodo={handleRemoveTodo} 
+      <TodoList
+        todos={todos}
+        onToggleTodo={handleToggleTodo}
+        onEditTodo={handleEditTodo}
+        onRemoveTodo={handleRemoveTodo}
       />
     </div>
   );
